3a19e2d9830757e80463fce689699e69779c63b3
AleVor <alevor657@gmail.com>
2018-05-17 21:44:54 +0200

    Updated tests US 1

--- src/test/java/com/bowling/FrameTest.java
+++ src/test/java/com/bowling/FrameTest.java
@@ -1,6 +1,5 @@
 package com.bowling;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
 import static org.junit.Assert.*;
 
@@ -15,15 +14,25 @@ public class FrameTest extends TestCase {
         Frame testFrame = new Frame();
 
         // A frame consists of 0 tries at the beginning
-        assertEquals(0, testFrame.tries.size());
+        assertEquals(0, testFrame.size());
         
-        testFrame.addResult(1);
+        testFrame.add(1);
 
-        assertEquals(1, testFrame.tries.size());
+        assertEquals(1, testFrame.size());
 
-        testFrame.addResult(5);
+        testFrame.add(5);
 
         // A frame consists of 2 tries after one round        
-        assertEquals(2, testFrame.tries.size());
+        assertEquals(2, testFrame.size());
+
+        // Make sure items are not added
+        testFrame.add(5);
+        testFrame.add(5);
+        testFrame.add(5);
+
+        // Make sure size is still 2 and consists of added items
+        assertEquals(2, testFrame.size());
+        assertEquals(1, testFrame.get(0));
+        assertEquals(5, testFrame.get(1));
     }
 }

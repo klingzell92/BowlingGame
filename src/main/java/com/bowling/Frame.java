ad34a55b43144617483403c7fcfc3e6c6bcbb03c
AleVor <alevor657@gmail.com>
2018-05-17 21:05:21 +0200

    implemented US 1

--- src/main/java/com/bowling/Frame.java
+++ src/main/java/com/bowling/Frame.java
@@ -1,6 +1,18 @@
 package com.bowling;
 
+import java.util.ArrayList;
+
+import com.bowling.exceptions.FrameEndException;
+
 class Frame
 {
-    
+    ArrayList<Integer> tries = new ArrayList<Integer>();
+
+    public Frame() {
+        
+    }
+
+    public void addResult(int score) {
+        this.tries.add(score);
+    }
 }

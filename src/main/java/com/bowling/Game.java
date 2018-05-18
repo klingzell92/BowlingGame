6b2ee972662cff60e7c20b5425b6b013f6a6fe81
AleVor <alevor657@gmail.com>
2018-05-18 17:22:33 +0200

    Implemented game

--- src/main/java/com/bowling/Game.java
+++ src/main/java/com/bowling/Game.java
@@ -1,6 +1,12 @@
 package com.bowling;
 
+import java.util.ArrayList;
+
 class Game 
 {
-    
+    public ArrayList<Frame> gameFrames = new ArrayList<Frame>();
+
+    public void addFrame(Frame f) {
+        gameFrames.add(f);
+    }
 }

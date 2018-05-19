5e881a45694744e011425926c65b7f416442fd3c
Philip Klingzell <philip_klingzell@hotmail.com>
2018-05-19 21:36:35 +0200

    Added test for US 7

--- src/test/java/com/bowling/GameTest.java
+++ src/test/java/com/bowling/GameTest.java
@@ -109,4 +109,52 @@ public class GameTest extends TestCase {
         assertEquals(97, g.getGameSum());
 
     }
+      /**
+     * Test that a game with a strike and a spare returns the correct sum
+     */
+    public void testGameSumWithSpareAndStrike(){
+        // US 7
+        Game g = new Game();
+        Frame frame = new Frame();
+
+        // Fill the game with a spare first and then a strike and the rest with 9s
+        frame.add(9);
+        frame.add(1);
+        g.addFrame(frame);
+        frame = new Frame();
+        frame.add(10);
+        frame.add(0);
+        g.addFrame(frame);
+        for (int i = 0; i < 8; i++) {
+            frame = new Frame();
+            frame.add(6);
+            frame.add(3);
+            g.addFrame(frame);
+        }
+
+        // Check that the total sum is correct
+        System.out.print(g.getGameSum() + "\n");
+        assertEquals(111, g.getGameSum());
+ 
+        // Fill the game with a spare first and then a strike and the rest with 9s
+        g = new Game();
+        frame = new Frame();
+        frame.add(10);
+        frame.add(0);
+        g.addFrame(frame);
+        frame = new Frame();
+        frame.add(9);
+        frame.add(1);
+        g.addFrame(frame);
+        for (int i = 0; i < 8; i++) {
+            frame = new Frame();
+            frame.add(6);
+            frame.add(3);
+            g.addFrame(frame);
+        }
+
+        // Check that the total sum is correct
+        assertEquals(108, g.getGameSum());
+
+    }
 }

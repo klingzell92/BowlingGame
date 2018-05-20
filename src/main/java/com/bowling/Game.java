93e93fb8b3bb3f2b47eccd1a59fae47e6e38e529
Philip Klingzell <philip_klingzell@hotmail.com>
2018-05-20 18:31:05 +0200

    Implemented US 8

--- src/main/java/com/bowling/Game.java
+++ src/main/java/com/bowling/Game.java
@@ -22,6 +22,12 @@ class Game
             if(ball1 == 10 & iteration < 9 ) {
                 // Add sum of the next frame to this frames sum
                 frameSum += gameFrames.get(iteration + 1).sum;
+
+                // If the next frame is a strike add score of the first thorw of the frame after
+                int nextFrameBall1 = (Integer) gameFrames.get(iteration + 1).get(0);
+                if(nextFrameBall1 == 10) {
+                    frameSum += (Integer) gameFrames.get(iteration + 2).get(0);
+                }
             } 
 
             // Check that the first throw is not a strike but the sum of the frame is 10 wich indicates that it is a spare
@@ -29,7 +35,6 @@ class Game
                 // Add score of the first throw of the next frame to this frames sum
                 frameSum += (Integer) gameFrames.get(iteration + 1).get(0);
             }
-
             sum += frameSum;
             iteration++;
         }
